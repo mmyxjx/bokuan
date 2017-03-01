8919adf74e964dc40ad158a4388a84091dcf7439
mmyxjx <649673002@qq.com>
2017-03-01 19:55:58 +0800

    test subMenu

--- src/components/MemuLeft.js
+++ src/components/MemuLeft.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Link } from 'react-router';
 import CreateDesire from './CreateDesire'
 import DesireList from "./DesireList"
-import {Menu,SubMenu} from 'antd'
+import {Menu, Switch} from 'antd'
+
+const SubMenu = Menu.SubMenu
 
 export default class MenuLeft extends React.Component{
     constructor(props) {
@@ -11,16 +13,13 @@ export default class MenuLeft extends React.Component{
     render(){
         return(
             <div className="d-left-side">
-                <ul className="d-main-menu">
-                    <li><Link to="/CreateDesire">创建</Link></li>
-                    <li><Link to="/DesireList">历史列表</Link></li>
-                </ul>
-                <Menu them="dark">
-                    <Menu.Item>菜单项</Menu.Item>
-                    <Menu.Item>菜单
-                    </Menu.Item>
-                    
-                    
+               <Menu theme="dark" mode="inline">
+                    <Menu.Item><Link to="/CreateDesire">创建</Link></Menu.Item>
+                    <Menu.Item><Link to="/DesireList">历史列表</Link></Menu.Item>
+                    <SubMenu  title="子菜单">
+                        <Menu.Item>子菜单项</Menu.Item>
+
+                    </SubMenu >
                 </Menu>
             </div>
             
